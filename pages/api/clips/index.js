0fb0803aa1f2bd2275dd84f5ca5440abd374218f
matthew <matthew@matthews-MacBook-Pro.local>
2021-11-29 20:16:51 -0500

    package install

--- pages/api/clips/index.js
+++ pages/api/clips/index.js
@@ -10,10 +10,7 @@ export default async(req, res) => {
         case "POST":
             try {
                 const clip = await Clip.create(req.body);
-                res.status(201).json({
-                    success: true,
-                    data: clip
-                })
+                res.status(201).json({success: true, data: clip })
             } catch (error) {
                 res.status(400).json({success: false})
             }
